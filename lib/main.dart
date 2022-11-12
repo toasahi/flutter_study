f97559223376b30fb041bf875edbd6cd384ad5e3
toasahi <asa1313to@gmail.com>
2022-11-12 16:01:42 +0900

    Section1: Image Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,13 @@ void main() {
       //デバックのバナーを隠す
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white70,
         appBar: AppBar(
           title: const Text("I am rich"),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'),
+            image: AssetImage('images/fish.png'),
           ),
         ),
       ),
