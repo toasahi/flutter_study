759b7f7120f7de31f54ae49f96a8b4ece8ffa90a
toasahi <asa1313to@gmail.com>
2022-11-12 15:44:19 +0900

    Section1: Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      //デバックのバナーを隠す
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white70,
+        appBar: AppBar(
+          title: const Text("I am rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'),
+          ),
+        ),
       ),
     ),
   );
