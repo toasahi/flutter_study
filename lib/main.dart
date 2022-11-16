5f9cd1e89c90858349fcd6958839c7e139b02a20
toasahi <asa1313to@gmail.com>
2022-11-16 16:37:25 +0900

    Section2: Icons Class

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,14 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blue,
         body: SafeArea(
           child: Column(
-            children: const [
-              CircleAvatar(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
                 radius: 50,
                 backgroundColor: Colors.white,
                 backgroundImage: AssetImage("images/fish.png"),
               ),
-              Text(
+              const Text(
                 "Fish",
                 style: TextStyle(
                   color: Colors.white,
@@ -33,13 +34,59 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'GloriaHallelujah',
                 ),
               ),
-              Text(
+              const Text(
                 "Flutter Developer",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
                   letterSpacing: 2,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(vertical: 10,horizontal: 25),
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "08-0000-0000",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(vertical: 10,horizontal: 25),
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.email,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "abcdefg@gmail.com",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
