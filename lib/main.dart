39c6d42fcf1e9a00dd7021ac67215a5d22e4795a
toasahi <asa1313to@gmail.com>
2022-11-16 14:27:50 +0900

    Section2: Container Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       //デバックのバナーを隠す
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("I am rich"),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/fish.png'),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: double.infinity,
+                child: const Text("Container1"),
+              ),
+              Container(
+                color: Colors.blue,
+                height: 100,
+                width: 100,
+                child: const Text("Container2"),
+              ),
+              Container(
+                color: Colors.green,
+                height: 100,
+                width: 100,
+                child: const Text("Container3"),
+              ),
+            ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
